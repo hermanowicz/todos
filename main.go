22046c8088cdb4e1f9b6d002c79e419fd4a37edc
Tomasz Hermanowicz <hermangti@gmail.com>
2025-02-08 22:14:18 +0100

    index works, script tag works, layout works :)

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html/v2"
+	"github.com/hermanowicz/todos/handlers"
+	"log"
+	"net/http"
+)
+
+var (
+	port = "8080"
+)
+
+//go:embed views/*
+var viewsfs embed.FS
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("Starting todos app on port:", port)
+
+	engine := html.NewFileSystem(http.FS(viewsfs), ".html")
+
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+		Prefork:               false,
+		CaseSensitive:         false,
+		Views:                 engine,
+	})
+
+	app.Use(logger.New())
+	app.Use(recover.New())
+
+	app.Get("/", handlers.GetIndex)
+
+	err := app.Listen(fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatal("Faild to start application with error: \n", err.Error())
+	}
 }
