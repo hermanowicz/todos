ae637e736f30a10647e37ebfa1554ea16520b4ab
Tomasz Hermanowicz <hermangti@gmail.com>
2025-02-28 14:59:53 +0100

    id for todo

--- migrations/todos.sql
+++ migrations/todos.sql
@@ -1,4 +1,4 @@
 create table todos
-(user varchar(50), todo_title varchar(100) not null, todo_body text, todo_list varchar(50), status integer default 0);
+(id varchar(30) primary key, user varchar(50), todo_title varchar(100) not null, todo_body text, todo_list varchar(50), status integer default 0);
 
 create index idx_user on todos(user);
