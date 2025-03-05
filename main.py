ec6d1e4ac58102783be695213c39a71d4559e5f2
Tomasz Hermanowicz <hermangti@gmail.com>
2025-03-05 17:56:36 +0100

    work in progress

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from todos!")
+from bottle import run, route, request, template, redirect, response
+from loguru import logger
+from itsdangerous import URLSafeSerializer
+import secrets
 
 
-if __name__ == "__main__":
-    main()
+# vars
+ss_secret_key = secrets.token_urlsafe(48)
+
+
+# setting up itsdangerous
+ss = URLSafeSerializer(ss_secret_key, 'auth')
+
+
+@route('/')
+def index():
+    return "hello, world -> index page"
+
+@route("/register")
+def register():
+    if request.method == "GET":
+        return "get register page"
+    if request.method == "POST":
+        return "form submit handler"
+
+@route("/login")
+def login():
+    if request.method == "GET":
+        return "login form response"
+    if request.method == "POST":
+        return "login form token sender"
+
+@route("/auth/<token>")
+def auth(token:str):
+    return f"token from url: {token}"
+
+@route('/privacy-policy')
+def privacy_policy():
+    return "privacy policy page"
+
+run(server='gunicorn', port=8080, host='0.0.0.0', debug=True, reloader='true')
---
+++ views/priavcy-policy.tpl
@@ -0,0 +1,5 @@
+%rebase('base.tpl', title='Privacy Policy | Todos')
+
+<h1 class="text-3xl bg-yellow-50">
+    Privacy Policy
+</h1>
