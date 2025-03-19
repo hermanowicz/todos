0383fe7d9cef4575f7b413bedc8493584813d73e
Tomasz Hermanowicz <hermangti@gmail.com>
2025-03-19 19:44:54 +0100

    fix to reloader.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from bottle import run, route, request, template, redirect, response
-from loguru import logger
-from itsdangerous import URLSafeSerializer
 import secrets
 
+from bottle import run, route, request
+from itsdangerous import URLSafeSerializer
 
 # vars
 ss_secret_key = secrets.token_urlsafe(48)
-
 
 # setting up itsdangerous
 ss = URLSafeSerializer(ss_secret_key, 'auth')
@@ -16,12 +14,14 @@ ss = URLSafeSerializer(ss_secret_key, 'auth')
 def index():
     return "hello, world -> index page"
 
+
 @route("/register")
 def register():
     if request.method == "GET":
         return "get register page"
     if request.method == "POST":
         return "form submit handler"
+
 
 @route("/login")
 def login():
@@ -30,12 +30,15 @@ def login():
     if request.method == "POST":
         return "login form token sender"
 
+
 @route("/auth/<token>")
-def auth(token:str):
+def auth(token: str):
     return f"token from url: {token}"
+
 
 @route('/privacy-policy')
 def privacy_policy():
     return "privacy policy page"
 
-run(server='gunicorn', port=8080, host='0.0.0.0', debug=True, reloader='true')
+
+run(server='gunicorn', port=8080, host='0.0.0.0', debug=True, reloader=True)
--- views/index.tpl
+++ views/index.tpl
@@ -1,1 +1,5 @@
 %rebase('base.tpl', title="Home sweet home | Todos")
+
+<h1 class="text-3xl bg-yellow-50 rounded shadow">
+    Hello to todos app in Bottle
+</h1>
